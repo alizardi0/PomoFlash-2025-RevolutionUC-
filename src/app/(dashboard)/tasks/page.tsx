c6c674acddb7249e3727991829a817a542f1f5ab
paulsparks <psparks1225@gmail.com>
2025-03-02 05:58:45 +0000

    hotfix ui

--- src/app/(dashboard)/tasks/page.tsx
+++ src/app/(dashboard)/tasks/page.tsx
@@ -7,7 +7,7 @@ import { CheckboxCard } from "@/components/ui/checkbox-card";
 
 export default function Tasks() {
   return (
-    <HStack mt="2em" w="fit" mx="auto" gapX={10}>
+    <HStack mt="4em" w="fit" mx="auto" gapX={10}>
       <TaskStack title="Tasks">
         <CheckboxCard variant="surface" colorPalette="green" label="Do the dishes" />
         <CheckboxCard variant="surface" colorPalette="green" label="Do the dishes" />
